fb72c5d7d8741373f029b37a65145e2c670f110c
Chectidus <chectidus@gmail.com>
2016-12-15 03:53:43 -0700

    Add files via upload

---
+++ Consultorio/Consultorio/Consultorio/Datos de proveedor.vb
@@ -0,0 +1,9 @@
+﻿Public Class Datos_de_proveedor
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Datos_de_proveedor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
